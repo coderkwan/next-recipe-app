90276480656efaf35767628297dcfb7cc2e5412c
kwanele <kwanelegamedze4@gmail.com>
2021-10-09 03:20:01 +0200

    busy with random card

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,22 @@ import Showcase from "../sections/Home/showcase/Index";
 import RandomMeal from "../sections/Home/randomMeals";
 import Categories from "../sections/Home/Cartegories";
 
-export default function MainComponent({ cateList }) {
+export default function MainComponent({ cateList, random, random2, random3 }) {
   const thelist = cateList.categories;
+
+  const mylist = [""];
+  mylist.push(random.meals[0]);
+  mylist.push(random2.meals[0]);
+  mylist.push(random3.meals[0]);
+  mylist.shift();
+  console.log(mylist);
 
   return (
     <div>
       {/* <Navbar /> */}
       <Showcase />
-      {/* <RandomMeal />
-      <Categories categories={cateList} /> */}
+      <RandomMeal random={mylist} />
+      {/* <Categories categories={cateList} /> */}
     </div>
   );
 }
@@ -20,11 +27,27 @@ export const getStaticProps = async () => {
   const data = await fetch(
     "https://www.themealdb.com/api/json/v1/1/categories.php"
   );
+  const myRandom = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/random.php"
+  );
+  const myRandom2 = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/random.php"
+  );
+  const myRandom3 = await fetch(
+    "https://www.themealdb.com/api/json/v1/1/random.php"
+  );
+
+  const random = await myRandom.json();
+  const random2 = await myRandom2.json();
+  const random3 = await myRandom3.json();
   const cateList = await data.json();
 
   return {
     props: {
       cateList,
+      random,
+      random2,
+      random3,
     },
   };
 };
--- sections/Home/randomMeals/index.js
+++ sections/Home/randomMeals/index.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import RandomMealCard from "../../../components/RandomMealCard";
 
-function RandomMeal() {
+function RandomMeal({ random }) {
   return (
     <div>
       <h1>I am the Random Meal</h1>
+      {random.map((item, key) => {
+        return <h5 key={key}>{item.strMeal}</h5>;
+      })}
     </div>
   );
 }
