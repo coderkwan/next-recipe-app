d5a73f7886e7880f34dd7ad7dd45741ddcda96d0
kwanele <kwanelegamedze4@gmail.com>
2021-10-11 03:32:41 +0200

    added footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,19 @@
-import React from "react";
+import styles from "../styles/forComponents/Footer.module.css";
 
 function Footer() {
-  return <div></div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <h4>
+          Meals<span>O</span>ff
+        </h4>
+        <div className={styles.links}>
+          <p>Categoties</p>
+          <p>Search by Area</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
