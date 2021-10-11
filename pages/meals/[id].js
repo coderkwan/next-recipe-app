b31feb5c16f1bb89acdf2bdc68065a1766e5c4f4
kwanele <kwanelegamedze4@gmail.com>
2021-10-11 15:30:41 +0200

    changed cards

--- pages/meals/[id].js
+++ pages/meals/[id].js
@@ -2,28 +2,12 @@ import Image from "next/dist/client/image";
 import styles from "../../styles/Recipes.module.css";
 import { useRouter } from "next/router";
 import PrimaryBtn from "../../components/PrimaryBtn";
+import RandomMealCard from "../../components/RandomMealCard";
 
 function Mypost({ result }) {
   const themeal = result.meals;
   const route = useRouter();
   const { id } = route.query;
-
-  const dataBlur = (w, h) => `
-      <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-        <defs>
-          <linearGradient id="g">
-            <stop stop-color="#7a6549" offset="20%" />
-            <stop stop-color="#787351" offset="50%" />
-            <stop stop-color="#665947" offset="70%" />
-          </linearGradient>
-        </defs>
-        <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-      </svg>`;
-
-  const toBase64 = (str) =>
-    typeof window === "undefined"
-      ? Buffer.from(str).toString("base64")
-      : window.btoa(str);
 
   return (
     <div>
@@ -31,28 +15,7 @@ function Mypost({ result }) {
         <h2 className={styles.title}>{id} Recipes</h2>
         <div className={styles.grid}>
           {themeal.map((item, key) => {
-            return (
-              <div key={key} className={styles.card}>
-                <div>
-                  <Image
-                    className="image"
-                    src={item.strMealThumb}
-                    placeholder="blur"
-                    blurDataURL={`data:image/svg+xml;base64,${toBase64(
-                      dataBlur(240, 240)
-                    )}`}
-                    height={240}
-                    width={240}
-                    alt="thumbnail"
-                  />
-                  <h4 className={styles.description}>{item.strMeal}</h4>
-                  <PrimaryBtn
-                    text="View Recipe"
-                    link={`/meals/details/${item.strMeal}`}
-                  />
-                </div>
-              </div>
-            );
+            return <RandomMealCard yourString="" Meal={item} key={key} />;
           })}
         </div>
       </div>
