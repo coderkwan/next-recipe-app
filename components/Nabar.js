c0b22641b001fa210c1a1f81463ce9bcf540ac08
kwanele <kwanelegamedze4@gmail.com>
2021-10-11 13:45:32 +0200

    CHANGED MENU DISPLAY

--- components/Nabar.js
+++ components/Nabar.js
@@ -1,23 +1,51 @@
 import style from "../styles/forComponents/nav.module.css";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import { useRouter } from "next/dist/client/router";
 import { categories } from "../data/Category";
+import { areas } from "../data/Category";
+import SearchArea from "./SearchArea";
 
-function Nabar() {
+function Nabar({ toggleArea }) {
   const [menu, setmenu] = useState(false);
+  const [click, setClick] = useState(false);
+
+  let display = "none";
+
+  const router = useRouter();
+  useEffect(() => {
+    router.events.on("routeChangeComplete", closeList);
+    return () => {
+      router.events.off("routeChangeComplete", closeList);
+    };
+  }, [router.events]);
+
+  const closeList = (e) => {
+    setClick(false);
+  };
+  const openList = (e) => {
+    setClick(true);
+  };
+
+  click ? (display = "flex") : (display = "none");
+
   function toggle() {
     setmenu(!menu);
   }
+  const categorytext = useRef();
+  const areatext = useRef();
 
-  let menuName = "Categories";
-  if (menu) {
-    menuName = "Close";
-  } else {
-    menuName = "Categories";
-  }
+  useEffect(() => {
+    if (menu) {
+      categorytext.current.style.color = "red";
+    } else {
+      categorytext.current.style.color = "black";
+    }
+  }, [menu]);
 
   return (
     <div className={style.wrapper}>
+      <SearchArea closeList={closeList} display={display} list={areas} />
       <div className={style.nav}>
         <Link href="/">
           <a className={style.home}>
@@ -25,8 +53,13 @@ function Nabar() {
           </a>
         </Link>
         <div className={style.navContainer}>
-          <h3 onClick={toggle} className={style.home}>
-            {menuName}
+          <h3 ref={areatext} onClick={openList} className={style.home}>
+            Search by Area
+          </h3>
+        </div>
+        <div className={style.navContainer}>
+          <h3 ref={categorytext} onClick={toggle} className={style.home}>
+            Categories
           </h3>
           {menu && (
             <ul className={style.list}>
