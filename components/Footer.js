85e3546cf2d8e35b76ed64d40d1a115d99ffe7cf
Kwanele Gamedze <kwanelegamedze4@gmail.com>
2023-04-14 17:04:04 +0200

    added creator name

--- components/Footer.js
+++ components/Footer.js
@@ -1,20 +1,28 @@
 import styles from "../styles/forComponents/Footer.module.css";
 import Link from "next/dist/client/link";
 function Footer() {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <Link href="/">
-          <p className={styles.home}>
-            Meals<span>O</span>ff
-          </p>
-        </Link>
-        <div className={styles.links}>
-          <p>Enjoy your Meals!</p>
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.container}>
+                <Link href="/">
+                    <p className={styles.home}>
+                        Meals<span>O</span>ff
+                    </p>
+                </Link>
+                <div>
+                    <p>
+                        Built by{" "}
+                        <a href="https://kwanele.netlify.app" target="blank">
+                            Kwanele Gamedze
+                        </a>
+                    </p>
+                </div>
+                <div className={styles.links}>
+                    <p>Enjoy your Meals!</p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Footer;
